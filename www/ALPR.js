35ad0615325de09b8149381e4e2ef5d0c5fa7902
Stijn De Haes <stijn.dehaes@rombit.be>
2016-09-30 14:08:01 +0200

    changed interface

--- www/ALPR.js
+++ www/ALPR.js
@@ -1,19 +1,9 @@
-var exec = require('cordova/exec');
-
-function ALPRPlugin() { 
- console.log("ALPR.js: is created");
-}
-
-ALPRPlugin.prototype.echoe = function(aString){
- console.log("ALPR.js: echo");
-
- exec(function(result){
-     /*alert("OK" + reply);*/
-   },
-  function(result){
-    /*alert("Error" + reply);*/
-   },"Echo","echo",["aString"]);
-}
-
- var alprPlugin = new ALPRPlugin();
- module.exports = alprPlugin;
+module.exports = {
+	echo: function(string, successCallback) {
+	    cordova.exec(successCallback,
+	                 null, // No failure callback
+	                 "Echo",
+	                 "echo",
+	                 [string]);
+	}
+};
